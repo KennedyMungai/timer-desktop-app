f209f2001b597b8093a31e4dc7dca85f4a546759
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-19 04:28:37 +0300

    Added a webkitAppRegion drag style to the component to be able to drag the app using this topbar component

--- src/renderer/src/components/top-bar.tsx
+++ src/renderer/src/components/top-bar.tsx
@@ -1,7 +1,7 @@
 const TopBar = () => {
   return (
     <div>
-      <div className="w-screen h-5 bg-blue-400 "></div>
+      <div className="w-screen h-5 bg-blue-400" style={{ webkitAppRegion: 'drag' }}></div>
     </div>
   )
 }
