2696ecd1d73f518ca0d3cb7b685ce3617b8d1bbc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-19 04:16:26 +0300

    Added some typescript-explicit-function return type rules for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,5 +5,11 @@ module.exports = {
     'plugin:react/jsx-runtime',
     '@electron-toolkit/eslint-config-ts/recommended',
     '@electron-toolkit/eslint-config-prettier'
-  ]
+  ],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': {
+      allowTypedFunctionExpressions: true,
+      allowFunctionsWithoutTypeParameters: true
+    }
+  }
 }
