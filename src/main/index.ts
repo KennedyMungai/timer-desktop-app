9888f5356ae521f551e929468d83c4286d420d8f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-19 04:19:42 +0300

    Removed the frame from the application

--- src/main/index.ts
+++ src/main/index.ts
@@ -8,6 +8,7 @@ function createWindow(): void {
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
+    frame: false,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
