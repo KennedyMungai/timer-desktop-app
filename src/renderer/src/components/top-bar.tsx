f96651760bbcf2e4f0c8d95ea4308c062375465d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-21 14:41:44 +0300

    Updated the height of a div inside the header component

--- src/renderer/src/components/top-bar.tsx
+++ src/renderer/src/components/top-bar.tsx
@@ -10,7 +10,7 @@ const TopBar = () => {
   return (
     <div>
       <div className="w-screen h-5 bg-blue-400 rounded-t-xl" style={{ webkitAppRegion: 'drag' }} />
-      <div className="w-screen h-5 bg-blue-400" />
+      <div className="w-screen h-8 bg-blue-400" />
       <div id="control-buttons" className="absolute right-0 top-2 pe-2">
         <button
           id="minimize"
