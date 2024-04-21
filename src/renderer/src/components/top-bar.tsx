b2ec59010173fc9361d2379dbfa0e242899e62ae
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-21 11:29:23 +0300

    Made some changes

--- src/renderer/src/components/top-bar.tsx
+++ src/renderer/src/components/top-bar.tsx
@@ -12,10 +12,18 @@ const TopBar = () => {
       <div className="w-screen h-5 bg-blue-400 rounded-t-xl" style={{ webkitAppRegion: 'drag' }} />
       <div className="w-screen h-5 bg-blue-400" />
       <div id="control-buttons" className="absolute right-0 top-2 pe-2">
-        <button id="minimize" onClick={handleMinimize} className="text-2xl font-semibold">
+        <button
+          id="minimize"
+          onClick={handleMinimize}
+          className="text-2xl font-semibold text-neutral-200 "
+        >
           &#95;
         </button>
-        <button id="close" onClick={handleClose} className="text-2xl font-semibold">
+        <button
+          id="close"
+          onClick={handleClose}
+          className="ml-2 text-2xl font-semibold text-neutral-200 "
+        >
           &times;
         </button>
       </div>
