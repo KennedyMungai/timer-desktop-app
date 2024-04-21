5904be9e8b632f7539e4dbd986b34051500eb2e1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-21 12:59:57 +0300

    Added all the necessary components to the input field components

--- src/renderer/src/components/timer.tsx
+++ src/renderer/src/components/timer.tsx
@@ -7,12 +7,21 @@ type Props = {
 
 const Timer = ({ isOverlay }: Props) => {
   const [isEditing, setIsEditing] = useState(false)
+  const [hours, setHours] = useState(0)
+  const [minutes, setMinutes] = useState(1)
+  const [seconds, setSeconds] = useState(0)
 
   return (
     <div>
-      <InputField label="Minutes" />
-      <InputField />
-      <InputField />
+      {isEditing ? (
+        <div>
+          <InputField label="Hours" value={hours} onChange={setHours} placeholder="Hours" />
+          <InputField label="Minutes" value={minutes} onChange={setMinutes} placeholder="Minutes" />
+          <InputField label="Seconds" value={seconds} onChange={setSeconds} placeholder="Seconds" />
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   )
 }
