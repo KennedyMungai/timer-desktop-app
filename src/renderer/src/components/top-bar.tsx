a28539c132412e9d45525cc535db45692ae75a9f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-21 11:18:06 +0300

    Added some event handler functions for when the minimize and close buttons are pressed

--- src/renderer/src/components/top-bar.tsx
+++ src/renderer/src/components/top-bar.tsx
@@ -1,7 +1,23 @@
 const TopBar = () => {
+  const handleClose = () => {
+    window.electron.ipcRenderer.send('close-window')
+  }
+
+  const handleMinimize = () => {
+    window.electron.ipcRenderer.send('minimize-window')
+  }
+
   return (
     <div>
       <div className="w-screen h-5 bg-blue-400" style={{ webkitAppRegion: 'drag' }}></div>
+      <div id="control-buttons" className="absolute top-0 right-0">
+        <button id="minimize" onClick={handleMinimize}>
+          &#95;
+        </button>
+        <button id="close" onClick={handleClose}>
+          &times;
+        </button>
+      </div>
     </div>
   )
 }
