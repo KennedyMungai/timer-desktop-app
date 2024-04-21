7dfc6422debd9061c8a137688aa5b71e9a1b0180
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-21 14:40:46 +0300

    Added some styles for the input field

--- src/renderer/src/components/input-field.tsx
+++ src/renderer/src/components/input-field.tsx
@@ -9,7 +9,13 @@ const InputField = ({ label, onChange, value, placeholder }: Props) => {
   return (
     <div>
       <label>{label}: </label>
-      <input type="number" value={value} onChange={onChange} placeholder={placeholder} />
+      <input
+        type="number"
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className="w-20 font-semibold bg-transparent text-sky-600"
+      />
     </div>
   )
 }
