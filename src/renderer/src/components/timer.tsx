934ea311b79af924ab867ba4eeb01a2909f415df
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-04-21 15:35:38 +0300

    Defined the stopwatch logic for the application

--- src/renderer/src/components/timer.tsx
+++ src/renderer/src/components/timer.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import InputField from './input-field'
 
 type Props = {
@@ -11,6 +11,33 @@ const Timer = ({ isOverlay }: Props) => {
   const [minutes, setMinutes] = useState<number>(1)
   const [seconds, setSeconds] = useState<number>(0)
   const [isActive, setIsActive] = useState(false)
+
+  useEffect(() => {
+    let intervalId
+
+    if (isActive)
+      intervalId = setInterval(() => {
+        if (seconds > 0) setSeconds((seconds) => seconds - 1)
+        else {
+          if (minutes === 0 && hours === 0) {
+            // TODO: Play audio
+            clearInterval(intervalId)
+            setIsActive(false)
+          } else {
+            if (minutes === 0) {
+              setHours((hours) => hours - 1)
+              setMinutes(59)
+            } else {
+              setMinutes((minutes) => minutes - 1)
+            }
+            setSeconds(59)
+          }
+        }
+      }, 1000)
+    else clearInterval(intervalId)
+
+    return () => clearInterval(intervalId)
+  }, [hours, seconds, minutes, isActive])
 
   return (
     <div>
@@ -51,13 +78,13 @@ const Timer = ({ isOverlay }: Props) => {
           <div id="timer-buttons" className="flex justify-around font-semibold text-slate-300">
             {isActive ? (
               <>
-                <button>Pause</button>
-                <button>Stop</button>
+                <button onClick={() => console.log('Something')}>Pause</button>
+                <button onClick={() => console.log('Something else')}>Stop</button>
               </>
             ) : (
               <>
-                <button>Start</button>
-                <button>Edit</button>
+                <button onClick={() => setIsActive(true)}>&#9658;</button>
+                <button onClick={() => setIsEditing(true)}>&#9998;</button>
               </>
             )}
           </div>
